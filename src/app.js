40dc4775a8ca2c2a1996c965ce83da8454949ca7
Luiz Boldrini <luizboldriniej@gmail.com>
2022-10-26 19:47:16 -0300

    Home page created

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const path = require("path")
 const routes = require('./app/routes/index')
 
 const app = express()
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "./public/"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+
 routes(app)
 
 
