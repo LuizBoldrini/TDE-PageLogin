f6d5ef7916d36d2e106ba10994dd6221acf7e4b1
Luiz Boldrini <luizboldriniej@gmail.com>
2022-10-26 21:33:07 -0300

    Implemented route POST

--- src/app/routes/login.js
+++ src/app/routes/login.js
@@ -1,9 +1,27 @@
 const express = require('express')
-
 const router = express.Router()
+require('dotenv').config();
 
-router.get('/', (req, res) => {
-    res.render('index')
-})
 
+let usuario = process.env.USUARIO
+let senha = process.env.SENHA
+
+router
+    .get('/', (req, res) => {
+        if(req.session.usuario) {
+            res.render('log', {usuario: usuario})
+        } else {
+            res.render('index')
+        }
+    })
+    .post('/', (req, res) => {
+        if(req.body.senha == senha && req.body.usuario == usuario){
+            req.session.usuario = usuario
+            
+            res.render('log', {usuario: usuario})
+        } else {
+            res.render('index')
+        }
+        
+    })
 module.exports = router
