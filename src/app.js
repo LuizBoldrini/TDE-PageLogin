c5d127022eb280a5334709c3ef175cc9133aec4a
Luiz Boldrini <luizboldriniej@gmail.com>
2022-10-26 21:32:33 -0300

    refatored

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const path = require("path")
 const routes = require('./app/routes/index')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(session({secret: '123456'}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("views", path.join(__dirname, "./public/"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
