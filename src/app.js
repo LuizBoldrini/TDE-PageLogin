45e88e62817a2bb2676e2a55a755a4b72d3048f1
Luiz Boldrini <luizboldriniej@gmail.com>
2022-11-01 20:59:59 -0300

    Refactored deprecated

--- src/app.js
+++ src/app.js
@@ -13,7 +13,11 @@ database.once('open', () => {
 const app = express()
 app.use(express.json());
 
-app.use(session({secret: '123456'}))
+app.use(session({
+  secret: '123456',
+  resave: true,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("views", path.join(__dirname, "./public/"));
 app.set("view engine", "ejs");
